6ef6e1cb5df035ecf1f59bc763f52967497ba1bf
Ольга Евстифеева <olga1989nikol@rambler.ru>
2022-09-17 00:13:55 +0300

    CardOrderFormTest changed

--- src/test/java/ru/netology/cardorderform/CardOrderFormTest.java
+++ src/test/java/ru/netology/cardorderform/CardOrderFormTest.java
@@ -1,5 +1,6 @@
 package ru.netology.cardorderform;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,7 @@ public class CardOrderFormTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();;
     }
 
     @BeforeEach
